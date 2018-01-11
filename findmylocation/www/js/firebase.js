a64c205c5d54b7148056deb15d2507bcb9c4488a
Sandro <sandro.trottmann@casasoft.ch>
2018-01-11 13:07:26 +0100

    j

--- findmylocation/www/js/firebase.js
+++ findmylocation/www/js/firebase.js
@@ -192,7 +192,22 @@
 
   	const li = document.getElementById('li');
   	li.innerText = snap.val();
+  	li.id = snap.key;
   	ulList.appendChild(li);
+
+  });
+
+  dbRefList.on('child_changed', snap => {
+
+  	const liChanged = document.getElementById(snap.key);
+  	liChanged.innerText = snap.val();
+
+  });
+
+  dbRefList.on('child_removed', snap => {
+
+  	const liToRemoved = document.getElementById(snap.key);
+  	liToRemoved.remove();
 
   });
 
